bcbaf5e0080fc4f4cd0f151436f507b770959eae
Michael Drogalis <mjd3089@rit.edu>
2016-03-11 15:06:42 -0800

    Invoke API ns.

---
+++ src/engraver_beginner_example/invoke_api.clj
@@ -0,0 +1,19 @@
+(ns engraver-beginner-example.invoke-api
+  (:require [engraver-beginner-example.sample-job :as s]
+            [engraver-beginner-example.configuration :as c])
+  (:gen-class))
+
+(defn -main [command job & args]
+  (cond (= command :submit-job)
+        (cond (= job :sample)
+              (s/-main)
+
+              :else
+              (println "Unknown job " job))
+
+        (= command :kill-job)
+        (let [peer-config (c/prod-peer-config)]
+          (onyx.api/kill-job peer-config job))
+
+        :else
+        (println "Unknown command " command)))
