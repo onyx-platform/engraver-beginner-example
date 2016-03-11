2ced0c311ee64f6bcaad3a3d089ee8190db22cc7
Michael Drogalis <mjd3089@rit.edu>
2016-03-11 12:22:40 -0800

    Switch underscores.

--- script/run_aeron.sh
+++ script/run_aeron.sh
@@ -3,4 +3,4 @@
 echo "Setting shared memory for Aeron"
 mount -t tmpfs -o remount,rw,nosuid,nodev,noexec,relatime,size=256M tmpfs /dev/shm
 
-exec java $MEDIA_DRIVER_JAVA_OPTS -cp /srv/engraver-beginner-example.jar "engraver-beginner-example.aeron_media_driver" >>/var/log/aeron.log 2>&1
+exec java $MEDIA_DRIVER_JAVA_OPTS -cp /srv/engraver-beginner-example.jar "engraver-beginner-example.aeron-media-driver" >>/var/log/aeron.log 2>&1
