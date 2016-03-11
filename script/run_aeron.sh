a3f3b55a1ef7acce180c23bc8a95f4607c0daa51
Michael Drogalis <mjd3089@rit.edu>
2016-03-11 13:46:50 -0800

    Fix logging.

--- script/run_aeron.sh
+++ script/run_aeron.sh
@@ -3,4 +3,4 @@
 echo "Setting shared memory for Aeron"
 mount -t tmpfs -o remount,rw,nosuid,nodev,noexec,relatime,size=256M tmpfs /dev/shm
 
-exec java $MEDIA_DRIVER_JAVA_OPTS -cp /srv/engraver-beginner-example.jar "engraver_beginner_example.aeron_media_driver" >>/var/log/aeron.log 2>&1
+exec java $MEDIA_DRIVER_JAVA_OPTS -cp /srv/engraver-beginner-example.jar "engraver_beginner_example.aeron_media_driver"
--- script/run_peers.sh
+++ script/run_peers.sh
@@ -4,4 +4,4 @@ set -o errexit
 set -o nounset
 set -o xtrace
 
-exec java $PEER_JAVA_OPTS -cp /srv/engraver-beginner-example.jar "engraver_beginner_example.launch_prod_peers" $NPEERS >>/var/log/onyx.log 2>&1
+exec java $PEER_JAVA_OPTS -cp /srv/engraver-beginner-example.jar "engraver_beginner_example.launch_prod_peers" $NPEERS
